a2d9c2356106eab3cd308dddc0ff8dd82c828754
megaboi2005 <57996646+megaboi2005@users.noreply.github.com>
2022-04-15 17:36:39 -0500

    added ores!!!
    flowers are in the code but are disabled since they are buggy
    Ores also generate weird when placed in a mountain

--- mcpiskyblock.py
+++ mcpiskyblock.py
@@ -18,63 +18,86 @@ noise = PerlinNoise(octaves=2, seed=int(seedinput))
 world = PerlinNoise(octaves=.1, seed=int(worldseed))
 heightmap = PerlinNoise(octaves=.1, seed=int(heightseed))
 plantsmap = PerlinNoise(octaves=10, seed=int(heightseed))
+ores = PerlinNoise(octaves=100, seed=int(seedinput))
 def tree(x,y,z,biome):
     treeheight = randrange(3,5)
-    
-    mc.setBlocks(x-1,y+treeheight,z-1,x+1,y+treeheight+1,z+1,18)
-    mc.setBlocks(x-2,y+treeheight-1,z-2,x+2,y+treeheight-2,z+2,18)
-    mc.setBlocks(x,y,z,x,y+treeheight,z,17)
-    
-def genisland(x,z,biome,size,height):
+    if biome == "plains":
+        mc.setBlocks(x-1,y+treeheight,z-1,x+1,y+treeheight+1,z+1,18)
+        mc.setBlocks(x-2,y+treeheight-1,z-2,x+2,y+treeheight-2,z+2,18)
+        mc.setBlocks(x,y,z,x,y+treeheight,z,17)
+    if biome == "desert":
+        mc.setBlocks(x,y,z,x,y+treeheight,z,81)
+def oregen(x,y,z,var):
+    height = randrange(1,2)
+    if var == 1:
+        mc.setBlocks(x,y,z,x,y-height,z,16)
+        #print("gen ore")
+    if var == 2:
+        mc.setBlocks(x,y,z,x,y-height,z,15)
+def genisland(x,z,size,height):
     freq=24
     amp = 10
     
-    if biome == "plain":
-        for x1 in range(size):
-            for z1 in range(size):
-                finalx = x1 + x
-                finalz = z1 + z
+    
+    for x1 in range(size):
+        for z1 in range(size):
+            finalx = x1 + x
+            finalz = z1 + z
+            ore = round(ores([finalx/freq,finalz/freq])*10)
+            #print(ore)
+            heightblock = round(world([finalx/freq,finalz/freq])*8)
+            heightmapblock = round(world([finalx/15,finalz/15])*100)
+            y1 = noise([finalx/freq,finalz/freq]) * heightmapblock
+            ynoheight = noise([finalx/freq,finalz/freq])
+            plants = round(plantsmap([finalx/freq,finalz/freq])*100)
+            #print(heightblock)
+            
+            if heightblock == 1:
+                #dirt
+                mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,13)
+                #stone
+                mc.setBlocks(finalx,y1,finalz,finalx,height,finalz,1)
                 
-                heightblock = round(world([finalx/freq,finalz/freq])*8)
-                heightmapblock = round(world([finalx/15,finalz/15])*300)
-                y1 = noise([finalx/freq,finalz/freq]) * heightmapblock
-                ynoheight = noise([finalx/freq,finalz/freq])
-                #print(heightblock)
                 
-
-                if heightblock == 1:
-                    #stone
-                    mc.setBlocks(finalx,y1,finalz,finalx,height,finalz,1)
-                    #dirt
-                    mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,13)
-                elif heightblock ==-1:
-                    #stone
-                    mc.setBlocks(finalx,y1,finalz,finalx,height,finalz,1)
-                    #dirt
-                    mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,12)
-                elif heightblock == 2:
-                    #stone
-                    mc.setBlocks(finalx,y1,finalz,finalx,height,finalz,1)
-                    #dirt
-                    mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,1)
-                    #grass
-                    mc.setBlock(finalx,y1,finalz,1)
-                else:
-                    #tree/plants
-                    plants = round(plantsmap([finalx/freq,finalz/freq])*100)
+            elif heightblock ==-1:
+                #stone
+                mc.setBlocks(finalx,y1,finalz,finalx,y1+height,finalz,1)
+                #sand
+                mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,12)
+                
+                
+                if plants == 2:
+                    time.sleep(.3)
+                    tree(finalx,y1+1,finalz,"desert")
+                
+            elif heightblock == 2:
+                #stone
+                mc.setBlocks(finalx,y1,finalz,finalx,height,finalz,1)
+                #dirt
+                mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,1)
+                #grass
+                mc.setBlock(finalx,y1,finalz,1)
+            else:
+                #tree/plants
+                
                     
-                    if plants == 2:
-                        tree(finalx,y1+1,finalz,"plains")
-                        
-                    #stone
-                    mc.setBlocks(finalx,y1,finalz,finalx,height,finalz,1)
-                    #dirt
-                    mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,3)
-                    #grass
-                    mc.setBlock(finalx,y1,finalz,2)
-genisland(0,0,"plain",8,-15)
+                
+                #stone
+                mc.setBlocks(finalx,y1,finalz,finalx,y1+height,finalz,1)
+                #dirt
+                mc.setBlocks(finalx,y1,finalz,finalx,y1+height/8,finalz,3)
+                #grass
+                mc.setBlock(finalx,y1,finalz,2)
+                if plants == 2:
+                    tree(finalx,y1+1,finalz,"plains")
+                #elif plants >=40:
+                 #   mc.setBlock(finalx,y1+1,finalz,37)
+                #elif plants >=20:
+                 #   mc.setBlock(finalx,y1+1,finalz,38)
+            oregen(finalx,y1+height+3,finalz,ore)
+genisland(0,0,8,-15)
 #tree(0,0,0,"plains")
 for a in range(islandcount):
-    print(f'generated {a} out of {islandcount}')
-    genisland(randrange(-128,128),randrange(-128,128),"plain",randrange(5,18),-7)
+    print(f'generated {a} out of {islandcount-1}')
+    genisland(randrange(-128,128),randrange(-128,128),randrange(5,18),-7)
     time.sleep(.5)
